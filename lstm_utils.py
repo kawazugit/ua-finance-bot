eb35b8733ec86cb2f5884e9e6388aa310155bdab
Markus Amano <markus.a.amano@gmail.com>
2021-03-20 11:45:37 -0500

    added an outline

---
+++ lstm_utils.py
@@ -0,0 +1,32 @@
+def fetch_data():
+    """
+    fetch_data() -> dataframes
+    This function downloads the latest financial data.
+    """
+    pass
+
+class LSTM_Model():
+    """
+    LSTM_Model()
+    This class is encapsulates the methods of a LSTM_Model.
+    The model should do the following:
+         ▫ Take in data and automatically split the training and test data sets
+         ▫ Should be able to generate visualizations
+         ▫ Should be able to save those visualizations
+         ▫ Can generate a performance report
+         ▫ Should be able to imported from a file. 
+    """
+    def __init__(self) -> None:
+        pass
+
+    def fit(self) -> None:
+        pass
+
+    def load(self,X,y) -> None:
+        pass
+
+    def save_model(self) -> None:
+        pass
+
+    def make_plot(self) -> None:
+        pass
